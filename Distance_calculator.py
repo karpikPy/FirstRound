572faa011423d42c04f6d4ffa777fee3d54d4fc0
karpikPy <karpikxd1212@gmail.com>
2024-09-30 20:04:35 +0300

    Update Distance_calculator.py
    Ментор предложил улучшить код с помощью dict так что я переработал код

--- Distance_calculator.py
+++ Distance_calculator.py
@@ -1,29 +1,33 @@
-print("Distance converter. Input distance in feet to convert into: inches, yards or miles")
+print("Distance converter. Input distance in feet to convert into: inches, yards, or miles")
 feet = float(input("Distance in feet: "))
 
-#dst means distance, for those who don't understand lmao.
+#dst is distance
 
-def Conversion():
+conversions = {
+    "inch": feet * 12,
+    "inches": feet * 12,
+    "foot": feet,
+    "feet": feet,
+    "yard": feet / 3,
+    "yards": feet / 3,
+    "mile": feet * 0.000189393939,
+    "miles": feet * 0.000189393939 # nah man wth is this number :skull:
+}
+
+def conversion():
     input_dst = str(input("Name of length unit: ")).lower()
-    if input_dst == "inch" or input_dst == "inches":
-        converted_dst = feet * 12
-        print(feet, "feet is equal to", converted_dst, "inches")
 
-    elif input_dst == "foot" or input_dst == "feet":
-        converted_dst = feet
-        print("NO WAY", feet, "FEET IS EQUAL TO", converted_dst, "FEET")
-        print("Who would've guessed?")
-
-    elif input_dst == "yard" or input_dst == "yards":
-        converted_dst = feet / 3
-        print(feet, "feet is equal to", converted_dst, "yards")
-
-    elif input_dst == "mile" or input_dst == "miles":
-        converted_dst = feet * 0.000189393939
-        rounded_up = converted_dst.__round__(3)
-        print(feet, "feet is equal to", rounded_up, "miles")
-
+    if input_dst in conversions:
+        converted_dst = conversions[input_dst]
+        if input_dst in ["mile", "miles"]:
+            rounded_up = round(converted_dst, 2)
+            print(feet, "feet is equal to", rounded_up, input_dst)
+        elif input_dst in ["foot", "feet"]:
+            print("NO WAY", feet, "FEET IS EQUAL TO", converted_dst, input_dst)
+            print("wow, who would've guessed?")
+        else:
+            print(feet, "feet is equal to", converted_dst, input_dst)
     else:
-        print("Wrong unit of length or misspelling. Skill issue from your side not gonna lie")
+        print("Mistake. Skill issue from your side not gonna lie")
 
-Conversion()
+conversion()
